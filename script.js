c43cc1d0c6d015ff539b895867863896502297a1
Anna Jeon <Anna@Annas-MacBook-Air.local>
2020-06-06 16:06:24 -0400

    commit update

--- script.js
+++ script.js
@@ -38,13 +38,62 @@ var pf = new petfinder.Client({
             console.log(error)
         });
 
-// function displayAnimals(dogData){
-//     for (i=0; i<dogData.data.animals.length; i++){
-//         if ( dogData.data.animals[i].age == "Baby" ){
-//             document.querySelector('h2').textContent += dogData.data.animals[i].name; 
+// function findDog(dogData) {
+//     var box = $("#dogBox");
+//     box.empty();
+
+//     for (var i = 0; i < dogData.data.animals.length; i++) {
+
+//         var dogInfoBox = $('<div class="col-sm-12 col-md-4">');
+//         var dogImg = $('<img>')
+//         var dogName = $('<h6>');
+//         var dogBreeds = $('<h6>');
+//         var dogColor = $('<h6>');
+//         var dogAge = $('<h6>');
+//         var dogDesc = $('<h6>');
+
+//         var noImg ="https://www.mcctoronto.com/wp-content/uploads/images/no-profile-picture-icon-15.png";
+//         var imgURL = dogData.data.animals[i].photos[0];
+        
+//         if (imgURL) {
+//             dogImg.attr("src", imgURL.medium)
+//         } else {
+//             dogImg.attr("src", noImg);
 //         }
+
+//         dogInfoBox.css("padding", "5px");
+//         dogName.text("Name: " + dogData.data.animals[i].name);
+//         dogBreeds.text("Breed: " + dogData.data.animals[i].breeds.primary);
+//         dogColor.text("Colors: " + dogData.data.animals[i].colors.primary);
+//         dogAge.text("Age: " + dogData.data.animals[i].age);
+//         dogDesc.text("Description: " + dogData.data.animals[i].description);
+//         dogInfoBox.append(dogImg, dogName, dogBreeds, dogColor, dogAge, dogDesc);
+//         box.append(dogInfoBox);
+        
 //     }
 // }
+
+function displayDog(dogData) {
+    for (var i = 0; i < dogData.data.animals.length; i++) {
+        var noImg ="https://www.mcctoronto.com/wp-content/uploads/images/no-profile-picture-icon-15.png";
+       
+        document.querySelector('#dogBox').innerHTML += 
+        `
+        <div class="col-sm-12 col-md-4">
+            <div class="card">
+                <img src="${ dogData.data.animals[i].photos.length>0 ? dogData.data.animals[i].photos[0].medium : noImg }" />
+                <br>Name: ${dogData.data.animals[i].name}
+                <br>Breed: ${dogData.data.animals[i].breeds.primary}
+                <br>Colors: ${dogData.data.animals[i].colors.primary}
+                <br>Age: ${dogData.data.animals[i].age}
+                <br>Description: ${dogData.data.animals[i].description}
+            </div>
+        </div>
+        `
+  }
+
+}
+
 
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
@@ -93,22 +142,3 @@ fetch("https://api.thedogapi.com/v1/breeds", requestOptions)
         }
     }
 }
-
-
-
-// function displayDogFact(dogfact){
-
-//     for (var i=0; i < dogfact.length ; i++){
-
-//         var searchedPet = document.querySelector('#searchedDog').value
-        
-//         console.log(searchedPet);
-
-//         if (dogfact[i].name === searchedPet){
-
-//             console.log(dogfact[i].origin)
-//         }
-
-        
-//     }
-// }
