c0d3291c79ff83178442487a875e0e3967e59a91
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-06-07 14:50:05 -0400

    pushed to master

--- script.js
+++ script.js
@@ -1,42 +1,73 @@
 // PetFinderAPI Request
-var pf = new petfinder.Client({
-    apiKey: "cb58oTV1HT1nHEGfDGeG9cJSxuW9OYdr4tFtullZNR8RPZDPcd", 
-    secret: "XgbRAAfgLmZGVKZE0Gj1GtDZMmqu39LFlNLdBrtA"});
 
-var dogData
+// ******* New Dog Search Test *******
+const pfAPI = new petfinder.Client({apiKey: "cb58oTV1HT1nHEGfDGeG9cJSxuW9OYdr4tFtullZNR8RPZDPcd", secret: "XgbRAAfgLmZGVKZE0Gj1GtDZMmqu39LFlNLdBrtA"});
 
-pf.animal.search({type: "Dog"})
-    .then(function(response) {
-    dogData = response;
-    console.log(dogData);
-    displayDog(dogData); 
-    });
+let apiResult, searchBreed;
 
-// Displays/Appends PetFinderAPI Results
-function displayDog(dogData) {
+async function main(){
+  // -- GET BREED LIST --
+  
+  apiResult = await pfAPI.animalData.breeds('dog')
+  console.log( `> ${apiResult.data.breeds.length} breeds found...` )
+  apiResult.data.breeds.forEach( function( breed ){
+    console.log( ` - ${breed.name}` )
+  } );
 
-    for (var i = 0; i < dogData.data.animals.length; i++) {
-        var noImg ="https://www.stma.org/wp-content/uploads/2017/10/no-image-icon.png";
-       
-        document.querySelector('#dogBox').innerHTML += 
-        `
+  // -- SHOW DOGS (of breed) --
+  searchBreed = document.querySelector('#searchDogAdopt').value;
+  console.log( `> looking for dogs of breed: ${searchBreed}:` )
+
+  document.querySelector('#dogBox').innerHTML = ""
+
+  let page = 1, firstPage = true
+  do {
+    apiResult = await pfAPI.animal.search({
+      type: "Dog", 
+      breed: searchBreed, page, limit: 100} );
+
+    if( firstPage ){
+      console.log( 
+        `=================================================\n`+
+        `.. got ${apiResult.data.animals.length} dawgs (total pages: ${apiResult.data.pagination ? apiResult.data.pagination.total_pages : '-' })`+
+        `=================================================\n`, apiResult )
+      firstPage = false;
+    } else {
+      console.log( ` .. got ${apiResult.data.animals.length} dawgs` )
+    }
+    
+    let dogIdx = (page-1)*100;
+    apiResult.data.animals.forEach( function( animal ){
+      console.log( ` -- ${++dogIdx}: ${animal.name} id: ${animal.id} url: ${animal.url}` )
+      const dogPhoto = !animal.primary_photo_cropped ? 
+        'https://img0.etsystatic.com/183/0/13221305/il_570xN.1214786404_1lgc.jpg' :
+        (animal.primary_photo_cropped.length ? animal.primary_photo_cropped[0].small : animal.primary_photo_cropped.small)
+        
+      document.querySelector('#dogBox').innerHTML += `
         <div class="col-sm-12 col-md-4">
             <div class="card">
-                <img src="${ dogData.data.animals[i].photos.length>0 ? dogData.data.animals[i].photos[0].medium : noImg }" style="height: 350px; width: 100%;"/>
-                ${dogData.data.animals[i].name ? `<br><strong>${dogData.data.animals[i].name}</strong>` : ``}
-                ${dogData.data.animals[i].breeds.primary ? `<br>Breed: ${dogData.data.animals[i].breeds.primary}` : ``}
-                ${dogData.data.animals[i].colors.primary ? `<br>Colors: ${dogData.data.animals[i].colors.primary}` : ``}
-                ${dogData.data.animals[i].age ? `<br>Age: ${dogData.data.animals[i].age}` : ``}
-                ${dogData.data.animals[i].gender ? `<br>Gender: ${dogData.data.animals[i].gender}` : ``}
-                ${dogData.data.animals[i].contact.email ? `<br>Email: ${dogData.data.animals[i].contact.email}` : ``}
-                ${dogData.data.animals[i].contact.phone ? `<br>Phone: ${dogData.data.animals[i].contact.phone}` : ``}
+                <img src="${ apiResult.data.animals[dogIdx].photos.length>0 ? apiResult.data.animals[dogIdx].photos[0].medium : noImg }" style="height: 350px; width: 100%;"/>
+                ${apiResult.data.animals[dogIdx].name ? `<br><div style="background-color: #ff9933; padding: 5px 0px"><strong>${apiResult.data.animals[dogIdx].name}</strong></div>` : ``}
+                ${apiResult.data.animals[dogIdx].breeds.primary ? `<br>Breed: ${apiResult.data.animals[dogIdx].breeds.primary}` : ``}
+                ${apiResult.data.animals[dogIdx].colors.primary ? `<br>Colors: ${apiResult.data.animals[dogIdx].colors.primary}` : ``}
+                ${apiResult.data.animals[dogIdx].age ? `<br>Age: ${apiResult.data.animals[dogIdx].age}` : ``}
+                ${apiResult.data.animals[dogIdx].gender ? `<br>Gender: ${apiResult.data.animals[dogIdx].gender}` : ``}
+                ${apiResult.data.animals[dogIdx].contact.email ? `<br>Email: ${apiResult.data.animals[dogIdx].contact.email}` : ``}
+                ${apiResult.data.animals[dogIdx].contact.phone ? `<br>Phone: ${apiResult.data.animals[dogIdx].contact.phone}` : ``}
+                <br>
+                <br>
+                <div class="container">
+                    <a href="${animal.url}" class="btn btn-outline-secondary">See ${apiResult.data.animals[dogIdx].name}</a>
+                </div>
             </div>
         </div>
-        `
-    }
+      `
+    })
+    // get next page
+    page++
 
+  } while( apiResult.data.pagination && apiResult.data.pagination.total_pages >= page )
 }
-
 
 // TheDogAPI Request
 var dogfact
@@ -87,6 +118,7 @@ function displayDogFact(dogfact){
                 </div>
                 <div class="col-sm-12 col-md-6" id="text" style="padding-top: 30px">
                     ${dogfact[i].name ? `<strong>${dogfact[i].name}</strong>` : ``}
+                    <br>
                     ${dogfact[i].breed_group ? `<br>Breed-Group: ${dogfact[i].breed_group}` : ``}
                     ${dogfact[i].origin ? `<br>Origin: ${dogfact[i].origin}` : ``}
                     ${dogfact[i].life_span ? `<br>Life-Span: ${dogfact[i].life_span}` : ``}
@@ -101,7 +133,7 @@ function displayDogFact(dogfact){
     }
 }
 
-// Function to find a picture based on ID of dog breed
+// Function to find a picture based on ID of dog breed HELLO SOMEONE LET ME IN ZOOM!
 async function searchPicture(picNum){
     console.log(`We are searching for your picture`)
     await fetch(`https://api.thedogapi.com/v1/images/search?breed_ids=${picNum}`, requestOptions)
